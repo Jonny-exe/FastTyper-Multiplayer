6b3a2233b412efcc46f7b77a07c2aafe0365de66
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-03-05 22:45:36 +0100

    feat: made a word type input

--- back/app.ts
+++ back/app.ts
@@ -9,66 +9,66 @@ require("source-map-support").install()
 const io = require("socket.io")(http, CORS)
 
 io.use((socket: any, next: any) => {
-  const username = socket.handshake.auth.username
-  if (!username) {
-    return next(new Error("invalid username"))
-  }
-  socket.username = username
-  next()
+	const username = socket.handshake.auth.username
+	if (!username) {
+		return next(new Error("invalid username"))
+	}
+	socket.username = username
+	next()
 })
 
 io.on("connection", async (socket: any) => {
-  const username = socket.username
-  let isLider: boolean
-  console.log(`${username} connected`)
-  await query(`insert into users (username, progress) values ($1, 0)`, [
-    username,
-  ])
+	const username = socket.username
+	let isLider: boolean
+	console.log(`${username} connected`)
+	await query(`insert into users (username, progress) values ($1, 0)`, [
+		username,
+	])
 
-  const users = await query("select username, progress from users")
-  if (users.length === 1) {
-    await query(`update text set lider = $1::text, quote = ''`, [username])
-    const sendInfo: Text = { lider: username, quote: "" }
-    isLider = true
-    io.emit("text", sendInfo)
-  }
+	const users = await query("select username, progress from users")
+	if (users.length === 1) {
+		await query(`update text set lider = $1::text, quote = ''`, [username])
+		const sendInfo: Text = { lider: username, quote: "" }
+		isLider = true
+		io.emit("text", sendInfo)
+	}
 
-  io.emit("users", users)
+	io.emit("users", users)
 
-  socket.on("update-progress", async ({ username, progress }: User) => {
-    query(`update users set progress = $1::bigint where username = $2::text`, [
-      progress,
-      username,
-    ])
-    const users = await query("select username, progress from users")
-    io.emit("users", users)
-  })
+	socket.on("update-progress", async ({ username, progress }: User) => {
+		query(`update users set progress = $1::int where username = $2::text`, [
+			progress,
+			username,
+		])
+		const users: User[] = await query("select username, progress from users")
+		io.emit("users", users)
+	})
 
-  socket.on("update-text", async ({ quote, lider }: Text) => {
-    await query(`update text set quote = $1::text where lider = $2::text`, [
-      quote,
-      lider,
-    ])
-    io.emit("text", { quote, lider })
-  })
+	socket.on("update-text", async ({ quote, lider }: Text) => {
+		await query(`update text set quote = $1::text where lider = $2::text`, [
+			quote,
+			lider,
+		])
+		io.emit("text", { quote, lider })
+	})
 
-  socket.on("disconnect", async () => {
-    await query(`delete from users where username = $1::text`, [username])
-    console.log(`${username} disconnected`)
-    if (isLider == true) {
-      let newLider = await query(
-        `select username from users where username <> $1::text`,
-        [username]
-      )
-      newLider = newLider.length > 0 ? newLider[0].username : ""
-      await query(`update text set lider = $1::text`, [newLider])
-      io.emit("text", { lider: newLider, quote: "" })
-    }
-    const newUsers = await query(`select username, progress from users`)
-    io.emit("users", newUsers)
-  })
+	socket.on("disconnect", async () => {
+		await query(`delete from users where username = $1::text`, [username])
+		console.log(`${username} disconnected`)
+		if (isLider == true) {
+			let newLider = await query(
+				`select username from users where username <> $1::text`,
+				[username]
+			)
+			newLider = newLider.length > 0 ? newLider[0].username : ""
+			await query(`update text set lider = $1::text`, [newLider])
+			io.emit("text", { lider: newLider, quote: "" })
+		}
+		const newUsers = await query(`select username, progress from users`)
+		io.emit("users", newUsers)
+	})
 })
 
 http.listen(4000, () => {
-  console.log("listening on *:4000")
+	console.log("listening on *:4000")
 })
--- front/src/components/Race.tsx
+++ front/src/components/Race.tsx
@@ -5,155 +5,166 @@ import { getNewQuote } from "../requests"
 import ProgressBar from "react-bootstrap/esm/ProgressBar"
 
 interface Props {
-  socket: any
-  username: string
+	socket: any
+	username: string
 }
 
 interface User {
-  username: string
-  progress: number
+	username: string
+	progress: number
 }
 
 interface Text {
-  quote: string
-  lider: string
+	quote: string
+	lider: string
 }
 
 interface Self {
-  username: string
-  progress: number
-  isLider: boolean
+	username: string
+	progress: number
+	isLider: boolean
 }
 
 const Race: React.FC<Props> = ({ socket, username }) => {
-  const input: any = useRef(null)
+	const input: any = useRef(null)
 
-  const [user, setUser] = useState<Self>({
-    username,
-    progress: 0,
-    isLider: false,
-  })
-  const [text, setText] = useState<Text>({ quote: "", lider: "" })
-  const [myText, setMyText] = useState<string>("")
-  const [isCorrect, setIsCorrect] = useState<boolean>(true)
-  const [isFinished, setIsFinished] = useState<boolean>(false)
-  const [users, setUsers] = useState<User[]>([])
+	const [user, setUser] = useState<Self>({
+		username,
+		progress: 0,
+		isLider: false,
+	})
+	const [text, setText] = useState<Text>({ quote: "", lider: "" })
+	const [myFullText, setMyFullText] = useState<string>("")
+	const [myWord, setMyWord] = useState<string>("")
+	const [isCorrect, setIsCorrect] = useState<boolean>(true)
+	const [isFinished, setIsFinished] = useState<boolean>(false)
+	const [users, setUsers] = useState<User[]>([])
 
-  const focus = () => {
-    input.current.focus()
-  }
+	const focus = () => {
+		input.current.focus()
+	}
 
-  useEffect(() => {
-    socket.on("text", (text: Text) => {
-      setText(text)
-      let { lider, quote } = text
-      if (lider === user.username) {
-        setUser({ ...user, isLider: true })
-      } else if (quote !== "") {
-        setIsFinished(false)
-      }
-      setMyText("")
-    })
-  }, [socket])
+	useEffect(() => {
+		socket.on("text", (text: Text) => {
+			setText(text)
+			let { lider, quote } = text
+			if (lider === user.username) {
+				setUser({ ...user, isLider: true })
+			} else if (quote !== "") {
+				setIsFinished(false)
+			}
+			setMyFullText("")
+		})
+	}, [socket])
 
-  useEffect(() => {
-    socket.on("users", (users: User[]) => {
-      setUsers(users)
-    })
-  }, [socket])
+	useEffect(() => {
+		socket.on("users", (users: User[]) => {
+			setUsers(users)
+		})
+	}, [socket])
 
-  useEffect(() => {
-    if (text.quote === myText) {
-      setIsFinished(true)
-      return
-    }
-    let correct = false
-    if (text.quote.indexOf(myText) === 0) {
-      correct = true
-    }
-    setIsCorrect(correct)
-  }, [myText]) // Maybe this is bad only "TEXT"
+	useEffect(() => {
+		//TODO: make this cleaner
+		if (text.quote === myFullText + myWord) {
+			setMyFullText((myFullText) => myFullText + myWord)
+			setIsFinished(true)
+			return
+		}
 
-  useEffect(() => {
-    socket.emit("update-progress", { progress: user.progress, username })
-  }, [user.progress, socket, username])
+		if (
+			myWord[myWord.length - 1] === " " &&
+			text.quote.indexOf(myFullText + myWord) === 0
+		) {
+			setMyFullText((myFullText) => myFullText + myWord)
+			setMyWord("")
+		}
 
-  useEffect(() => {
-    socket.emit("update-text", text)
-  }, [text.quote, socket])
+		let correct = text.quote.indexOf(myFullText + myWord) === 0
+		setIsCorrect(correct)
+	}, [myWord]) // Maybe this is bad only "TEXT"
 
-  const newQuote = async () => {
-    setMyText("")
-    const newQuote = await getNewQuote()
-    setText({ ...text, quote: newQuote })
-    focus()
-    setIsFinished(false)
-  }
+	useEffect(() => {
+		socket.emit("update-progress", { progress: user.progress, username })
+	}, [user.progress, socket, username])
 
-  const haveUsersFinished = useMemo(() => {
-    let finished: boolean = true
-    if (text.quote === "") return true
-    users.every(({ progress }: User) => {
-      if (progress != 100) {
-        finished = false
-        return false
-      }
-      return true
-    })
-    debugger
-    return finished
-  }, [users, text.quote])
+	useEffect(() => {
+		socket.emit("update-text", text)
+	}, [text.quote, socket])
 
-  useEffect(() => {
-    const progress = Math.floor(
-      isFinished
-        ? 100
-        : (100 * (myText.split(" ").length - 1)) / text.quote.split(" ").length
-    )
-    setUser({ ...user, progress })
-  }, [myText, text.quote, isFinished])
+	const newQuote = async () => {
+		//TODO: make a timer for when you stanrt and you finish
+		setMyFullText("")
+		setMyWord("")
+		const newQuote = await getNewQuote()
+		setText({ ...text, quote: newQuote })
+		focus()
+		setIsFinished(false)
+	}
 
-  console.log(haveUsersFinished)
+	const haveUsersFinished = useMemo(() => {
+		let finished: boolean = true
+		if (text.quote === "") return true
+		users.every(({ progress }: User) => {
+			if (progress !== 100) {
+				finished = false
+				return false
+			}
+			return true
+		})
+		return finished
+	}, [users, text.quote])
 
-  return (
-    <div className="race">
-      <Form.Group
-        // onChange={(e: any) => setRace(e.target.value)}
-        controlId="exampleForm.ControlmyTextarea1"
-      >
-        <Form.Label>
-          {" "}
-          <h2>{text.quote}</h2>{" "}
-        </Form.Label>
-        <Form.Control
-          readOnly={isFinished}
-          value={myText}
-          onChange={(e: any) => setMyText(e.target.value)}
-          as="textarea"
-          className={`${isFinished ? "" : isCorrect ? "correct" : "incorrect"}`}
-          rows={3}
-          ref={input}
-        />
-      </Form.Group>
+	useEffect(() => {
+		const progress = Math.floor(
+			isFinished
+				? 100
+				: (100 * (myFullText.split(" ").length - 1)) /
+						text.quote.split(" ").length
+		)
+		setUser({ ...user, progress })
+	}, [myFullText, text.quote, isFinished])
 
-      <ProgressBar className="bar" now={user.progress} />
-      <Button
-        disabled={!user.isLider || !haveUsersFinished}
-        onClick={newQuote}
-        variant="primary"
-      >
-        {" "}
-        New quote{" "}
-      </Button>
-      {isFinished ? <h1> GG </h1> : null}
-      <hr></hr>
-      {users.map(({ username, progress }: User, index: number) => (
-        <div key={index}>
-          <h2> Username: {username} </h2>
-          <ProgressBar className="bar" now={progress} />
-        </div>
-      ))}
-    </div>
-  )
+	return (
+		<div className="race">
+			<Form.Group controlId="exampleForm.ControlmyTextarea1">
+				<Form.Label>
+					<h2>
+						{text.quote.substring(0, myFullText.length)}
+						<span className="word">|</span>
+						{text.quote.substring(myFullText.length, text.quote.length)}
+					</h2>
+				</Form.Label>
+				<Form.Control
+					readOnly={isFinished}
+					value={myWord}
+					onChange={(e: any) => setMyWord(e.target.value)}
+					as="textarea"
+					size="lg"
+					className={`${isFinished ? "" : isCorrect ? "correct" : "incorrect"}`}
+					rows={1}
+					ref={input}
+				/>
+			</Form.Group>
+
+			<ProgressBar className="bar" now={user.progress} />
+			<Button
+				disabled={!user.isLider || !haveUsersFinished}
+				onClick={newQuote}
+				variant="primary"
+			>
+				New quote
+			</Button>
+			{isFinished ? <h1> GG </h1> : null}
+			<hr></hr>
+			{users.map(({ username: name, progress }: User, index: number) =>
+				username === name ? null : (
+					<div key={index}>
+						<h2> Username: {name} </h2>
+						<ProgressBar className="bar" now={progress} />
+					</div>
+				)
+			)}
+		</div>
+	)
 }
 export default Race
