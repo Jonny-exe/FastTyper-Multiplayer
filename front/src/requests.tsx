a8195b81f47ac8b53b102469ca8593bf5ed884fa
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-03-07 10:58:48 +0100

    chore: added check for when the quote api fails

--- front/src/requests.tsx
+++ front/src/requests.tsx
@@ -1,6 +1,13 @@
 import axios from "axios"
 
 export const getNewQuote = async () => {
-  const res = await axios.get("https://api.quotable.io/random")
-  return res.data.content
+	let result: string
+	try {
+		const res = await axios.get("https://api.quotable.io/random")
+		result = res.data.content
+	} catch (err) {
+		console.error("Error getting new quote: ", err)
+		result = "The quick brown fox jumps over the lazy dog"
+	}
+	return result
 }
